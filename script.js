f6566c2fcb2c11365f4f09a537eacf45d92601cd
mstrnilsson <97398930+mstrnilsson@users.noreply.github.com>
2024-10-06 18:06:32 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ function showTime() {
   var fecha = new Date();
   var hora = new Date();
   fecha = fecha.toLocaleDateString("es-ES", { weekday:'long', day:'numeric', month:'long', year:'numeric'});
-  hora = hora.toLocaleTimeString()
+  //hora = hora.toLocaleTimeString()
   
-  document.getElementById('currentTime').innerHTML = fecha + " " +hora;
+  document.getElementById('currentTime').innerHTML = fecha //+ " " + hora;
 }
 showTime();
 setInterval(function () {
