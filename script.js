19c38453bd50a97b7725a580fef3013e9a9eb766
mstrnilsson <97398930+mstrnilsson@users.noreply.github.com>
2024-10-06 18:17:44 +0200

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function showTime() {
   hora = hora.toLocaleTimeString();
   
   //document.getElementById('currentTime').innerHTML = fecha;
-	console.log(fecha + " " + hora);
+	alert(fecha + " " + hora);
   //document.getElementById('currentTime').innerHTML = hora;
 }
 showTime();
